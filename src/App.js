87b5148591cbae183ebf661235331fb0f139a4b0
prateekmlg-1907 <prateekmalagund@gmail.com>
2023-08-14 17:32:53 +0530

    Add

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
-// import React from 'react';
-// import "bootstrap/dist/css/bootstrap.min.css";
-
-// function App() {
-//   return (
-//     // 1st Method
-//     // <div>
-//     //  <h1>React</h1>
-//     //  <p>Hello Everyone.</p>
-//     // </div>
-//     // 2nd Method
-//     <>
-//       <h1 className='bg-primary'>React</h1>
-//       <p>Hello Everyone.</p>
-//     </>
-//   )
-// }
-
-// export default App
-
-
 import React from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./Components/Home";
 import About from "./Components/About";
-import Courses from "./Components/Courses";
 import NavBar from "./Components/NavBar";
 import Login from "./Components/Login";
 import Signup from "./Components/Signup";
+import Search from "./Components/Search";
+import Menu from "./Components/Menu";
+import Create from "./Components/Create";
 
 
 function App() {
@@ -36,11 +17,13 @@ function App() {
       <NavBar />
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/courses" element={<Courses />} />
             <Route path="/contact" element={<About />} />
             <Route path="/" element={<NavBar />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/create" element={<Create />} />
             
 
         </Routes>
